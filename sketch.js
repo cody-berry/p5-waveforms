a199bc8ca89b52bc8bbe83b42f316393a5c3311e
Cody <cody.tian@icloud.com>
2021-10-04 11:14:03 -0700

    add version comments

---
+++ sketch.js
@@ -0,0 +1,26 @@
+/*
+waveforms
+by Cody
+at October 04, 2021
+inspired by Zz and his physics video
+version comments:
+
+
+
+
+
+ */
+let font
+
+function preload() {
+    font = loadFont('fonts/Meiryo-01.ttf')
+}
+
+function setup() {
+    createCanvas(640, 360)
+    colorMode(HSB, 360, 100, 100, 100)
+}
+
+function draw() {
+    background(209, 80, 30)
+}
